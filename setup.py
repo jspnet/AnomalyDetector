12f602ff07f9414caee5d6da2677e5d5c80bbed7
Masahiko Hashimoto <hashimom@geeko.jp>
2021-11-04 21:00:06 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
     name='anomalydetector',
     version='https://github.com/jspnet/AnomalyDetector',
-    packages=['anomalydetector'],
+    packages=find_packages(),
     url='',
     install_requires=[
         'tensorflow',
